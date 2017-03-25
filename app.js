521bda7b7491b6d1c7b127ed94323a03204375e4
dungqq <ntdung012@gmail.com>
2017-03-25 05:50:07 +0000

    Update port application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 //Routes
@@ -14,6 +15,6 @@ app.get('/star_wars_episode/:episode_number', routes.movie_single);
 
 //notFound
 app.get('*',routes.notFound);
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log('The application is running on localhost:3000');
 })
